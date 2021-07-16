83c36674351cb604b1ad28207bed48574c36a2db
rhonner <rhonner.matheus@gmail.com>
2021-07-16 19:06:30 -0300

    projeto finalizado

---
+++ lib/screens/contacts_list.dart
@@ -0,0 +1,87 @@
+import 'package:bytebank2/database/dao/contact_dao.dart';
+import 'package:bytebank2/models/contact.dart';
+import 'package:bytebank2/screens/contact_form.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ContactsList extends StatefulWidget {
+  @override
+  _ContactsListState createState() => _ContactsListState();
+}
+
+class _ContactsListState extends State<ContactsList> {
+  final ContactDao _contactDao = new ContactDao();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contacts'),
+      ),
+      body: FutureBuilder<List<Contact>>(
+        initialData: [],
+        future: _contactDao.findAll(),
+        builder: (context, AsyncSnapshot<List<Contact>> snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              return Center(child: CircularProgressIndicator());
+            case ConnectionState.active:
+              return Center(child: CircularProgressIndicator());
+            case ConnectionState.done:
+              final contacts = snapshot.data!;
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final Contact contact = contacts[index];
+                  return _ContactItem(contact);
+                },
+                itemCount: contacts.length,
+              );
+          }
+
+          return Text('clausula impossivel');
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context)
+              .push(
+            MaterialPageRoute(
+              builder: (context) => ContactForm(),
+            ),
+          )
+              .then((value) {
+            setState(() {});
+          });
+        },
+        child: Icon(
+          Icons.add,
+        ),
+      ),
+    );
+  }
+}
+
+class _ContactItem extends StatelessWidget {
+  final Contact contact;
+
+  _ContactItem(this.contact);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          contact.name,
+          style: TextStyle(fontSize: 24.0),
+        ),
+        subtitle: Text(
+          contact.accountNumber.toString(),
+          style: TextStyle(fontSize: 16.0),
+        ),
+      ),
+    );
+  }
+}
